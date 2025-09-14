0bf3eec7599556014d105830825ac6ff2dbf183e
Colton Rizzo <rizzocolton@gmail.com>
2025-09-14 18:29:27 -0400

    title/first card created, ready to switch screens

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,16 @@
 import React from 'react';
+//Component imports
+import Header from './Header/Header.tsx'
 import Card from './Card/Card.tsx'
+//Image imports
+import physiSimLogo from './assets/physisimlogo.png'
 
 function App() {
   return(
     <>
-      <Card/>
-      <Card/>
+      <Header/>
+      <Card img={physiSimLogo} title="Kinematics" desc="A simple single object simulation of projectile motion in a uniform gravitational field"/>
+
     </>
   );
 }
--- frontend/src/Card/Card.tsx
+++ frontend/src/Card/Card.tsx
@@ -1,13 +1,19 @@
 import React from 'react'
-import physiSimLogo from '../assets/physisimlogo.png'
 import styles from './Card.module.css'
 
-function Card(){
+type CardProps={
+   desc:string; 
+   title:string;
+   img:string;
+}
+
+function Card(props:CardProps){
     return(
         <div className={styles['card']}>
-            <img className={styles['card-image']} src={physiSimLogo} alt="kinematics sim picture"></img>
-            <h2 className={styles['card-title']}>Kinematics</h2>
-            <p className={styles['card-desc']}>A simple single object simulation of projectile motion in a uniform gravitational field</p>
+            <img className={styles['card-image']} src={props.img} alt="kinematics sim picture"></img>
+            <h2 className={styles['card-title']}>{props.title}</h2>
+            <hr color='black'></hr>
+            <p className={styles['card-desc']}>{props.desc}</p>
         </div>
     );
 }
