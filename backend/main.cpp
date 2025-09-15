69f0ce9dc608da88aee3971814bf7d42f3702e11
Colton Rizzo <rizzocolton@gmail.com>
2025-09-15 01:10:00 -0400

    mildly broken, still linking emsdk js output to react

---
+++ backend/main.cpp
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <emscripten.h>
+
+EMSCRIPTEN_KEEPALIVE
+int main(){
+    printf("hello, world\n");
+    return 0;
+}
--- frontend/src/Simulators/Kinematics.tsx
+++ frontend/src/Simulators/Kinematics.tsx
@@ -1,8 +1,39 @@
 import Header from '../Header/Header';
+import {useEffect,useState} from 'react';
 
 function Kinematics(){
+/*const [wasmReady, setWasmReady] = useState(false);
+
+  useEffect(() => {
+    const script = document.createElement('script');
+    script.src = '/main.out.js';
+    script.onload = () => {
+      // Wait for Module to be ready
+      window.Module().then(() => {
+        setWasmReady(true);
+        console.log("WASM ready!");
+      });
+    };
+    document.head.appendChild(script); // This was missing!
+  }, []);
+
+  function clickHandler() {
+    if (wasmReady && window.Module) {
+      window.Module.ccall('main', null, [], []);
+    }
+  }*/
+
+  
+
     return(
+        <>
+        <head>
+            <script src="/main.out.js"></script>
+        </head>
         <Header/>
+        
+        <button onClick={Module.ccall("main",null,[],[])} disabled={!wasmReady}>TEST BUTTON</button>
+        </>
     );
 }
 
